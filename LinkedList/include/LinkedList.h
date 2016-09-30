b6f9ca44498d7bb2b30181b31fc71c4b382fc215
Steven Ward <stevenward94@gmail.com>
2016-09-30 18:38:39 -0500

    Initial Commit & Add draft of 'LinkedList/include/LinkedList.h'

---
+++ LinkedList/include/LinkedList.h
@@ -0,0 +1,60 @@
+/***********************************************************************************//**
+ * Author:        Steven Ward <stevenward94@gmail.com>
+ * Project:       LinkedList
+ * File:          include/LinkedList.h
+ * Last Change:   2016 Sep 30
+ **************************************************************************************/
+
+#ifndef INCLUDE_LINKEDLIST_H_
+#define INCLUDE_LINKEDLIST_H_
+
+// #include <iterator>
+
+template <class T>
+class LinkedList {
+
+private:
+  struct NodeType {
+    T data;
+    NodeType* next;
+    NodeType(const T& t) : data(t), next(nullptr) { }
+    NodeType(const NodeType& init) : data(init.data), next(nullptr) { }
+  };
+
+  NodeType* head_;
+  std::size_t nNodes_;
+
+public:
+  inline NodeType* head() const { return head_; }
+  NodeType* head(NodeType* head);
+
+  inline std::size_t size() const { return nNodes_; }
+  inline bool isEmpty() const { return !head_; }
+
+  LinkedList<T>& appendNode(const T& data);
+  LinkedList<T>& prependNode(const T& data);
+  LinkedList<T>& insertNode(const T& data, const std::size_t index);
+
+  LinkedList<T>& replaceNode(const T& newData, const T& old, const std::size_t pos = 0);
+  LinkedList<T>& replaceNodeAt(const T& data, const std::size_t index);
+
+  LinkedList<T>& deleteNode(const T& data);
+  LinkedList<T>& deleteNode(const std::size_t index);
+
+  std::size_t find(const T& value, const std::size_t pos = 0) const;
+  std::size_t count(const T& value, const std::size_t pos = 0) const;
+  T& at(const std::size_t index) const;
+  NodeType& operator[] (std::size_t index);
+
+  inline LinkedList<T>() : nNodes_(0), head_(nullptr) { }
+  LinkedList<T>(const NodeType* head);
+  LinkedList<T>(const T& data);
+
+  template <class InputIterator>
+  LinkedList<T>(InputIterator first, InputIterator last);
+
+  ~LinkedList<T>();
+
+};
+
+#endif  // INCLUDE_LINKEDLIST_H_
---
+++ ProgrammingAssignment2/head/dayType.h
@@ -0,0 +1,16 @@
+/**
+ * Steven Ward
+ * Programming Assignment 2 - Chapter 1, Exercise 2
+ * 09.13.2016
+ *
+ * Definition for the dayType class
+ */
+
+#ifndef DAY_TYPE_H
+#define DAY_TYPE_H
+
+class dayType {
+
+};
+
+#endif
