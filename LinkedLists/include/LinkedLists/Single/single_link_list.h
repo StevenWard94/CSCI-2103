f8be4734afc4eec8e973981b9f7415848ca0dd10
Steven Ward <stevenward94@gmail.com>
2016-10-10 00:31:36 -0500

    Remove 'LinkedList/' directory. Add 'LinkedLists/' directory with almost-bare file, 'include/LinkedLists/Single/single_link_list.h'

---
+++ LinkedLists/include/LinkedLists/Single/single_link_list.h
@@ -0,0 +1,14 @@
+/***************************************************************************//**
+ * File:        single_link_list.h
+ * Path:        <project-root>/include/LinkedLists/Single
+ *
+ * Author:      Steven Ward <stevenward94@gmail.com>
+ * URL:         https://github.com/StevenWard94/csci-2103.git
+ * Last Change: 2016 Oct 10
+ ******************************************************************************/
+
+#ifndef LINKED_LISTS_SINGLE_SINGLE_LINK_LIST_H_
+#define LINKED_LISTS_SINGLE_SINGLE_LINK_LIST_H_
+
+
+#endif
