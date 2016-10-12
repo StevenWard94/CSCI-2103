3558615e657ab7eb990da626905f6039cfa10694
Steven Ward <stevenward94@gmail.com>
2016-10-12 16:12:49 -0500

    Finish class definition, and begin implementation, in 'src/SingleLinkList.cpp'

--- LinkedLists/src/SingleLinkList.cpp
+++ LinkedLists/src/SingleLinkList.cpp
@@ -2,27 +2,28 @@
  * file:          <project-root-dir>/src/SingleLinkList.cpp
  * author:        Steven Ward <stevenward94@gmail.com>
  * url:           https://github.com/StevenWard94/csci-2103.git
- * last change:   2016 Oct 11
+ * last change:   2016 Oct 12
 ****************************************************************************/
 
 #ifndef SRC_SINGLE_LINK_LIST_CPP_
 #define SRC_SINGLE_LINK_LIST_CPP_
 
 #include <iostream>
+#include <utility>
 
 #include <stddef.h>
 
-namespace linked_lists {
+namespace single_link {
 
-template <class T>
+template<class T>
 class List {
-  class Node_t;
+  class node_t;
  public:
   static const size_t NPOS = -1;
 
   /** inline accessor functions for private instance members */
-  inline Node_t* head( ) const { return this->head_; }
-  inline Node_t* tail( ) const { return this->tail_; }
+  inline node_t* head( ) const { return this->head_; }
+  inline node_t* tail( ) const { return this->tail_; }
   inline size_t  size( ) const { return this->size_; }
 
   inline bool isEmpty( ) const {
@@ -48,23 +49,38 @@ class List {
   List<T>& remove(size_t index);
   List<T>& remove(T const& data, size_t n = 1, size_t pos = 0);
   List<T>& removeAll(T const& data, size_t pos = 0);
-  List<T>& erase(size_t first_incl = 0, size_t last_excl = NPOS);
+  List<T>& erase(size_t first = 0, size_t last = NPOS);
 
   inline List( ) : head_(nullptr), tail_(nullptr), size_(0) { }
-  List(T const& data);
-  List(Node_t const* first_node);
-  // TODO: DEFINE MORE CONSTRUCTORS
+  List(T const& data, node_t* init_elem = nullptr);
+  List(size_t init_size, T const& init_data = T());
+  List(List<T> const& init);
+  template<class InputIterator>
+  List(InputIterator first, InputIterator last);
   ~List( );
 
+  List<T>& operator=(List<T> rhs_copy);
+  void swap(List<T>& rhs) noexcept;
+
  private:
-  Node_t* head_;
-  Node_t* tail_;
+  node_t* head_;
+  node_t* tail_;
   size_t  size_;
+
+  template<class U> friend std::ostream& operator<<(std::ostream& , List<U> const& );
+  template<class U> friend std::istream& operator>>(std::istream& , List<U>& );
+
+};
+
+template<class T>
+class node_t {
+ public:
+  T data;
+  node_t* next;
+
+  node_t(T const& init_data = T(), node_t* const init_next = nullptr)
+    : data(init_data), next(init_next) { }
 };
 }
 
 #endif    // (SRC_SINGLE_LINK_LIST_CPP_)
-
-namespace linked_lists {
-// TODO: implementation for List<T> class
-}
