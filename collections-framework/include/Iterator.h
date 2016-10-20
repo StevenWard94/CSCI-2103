0e64dc179e7a93d129d1cfa776071ade863aea3c
Steven Ward <stevenward94@gmail.com>
2016-10-19 19:26:32 -0500

    Add 'collections-framework' directory and project subdirectories. Add 'include/Iterator.h' to define the 'Iterator' class template

---
+++ collections-framework/include/Iterator.h
@@ -0,0 +1,6 @@
+/***************************************************************************//**
+ * Author:       Steven Ward <stevenward94@gmail.com>
+ * File:         <repository-root-dir>/collections-framework/include/Iterator.h
+ * URL:          https://github.com/StevenWard94/csci-2103/tree/collections-framework
+ * Last Change:  2016 Oct 19
+ ***************************************************************************/
