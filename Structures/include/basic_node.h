1e6241b5426c9dfd208821058ab5896cba31479e
Steven Ward <stevenward94@gmail.com>
2016-10-26 12:36:09 -0500

    Purge 'Structures/*'. Add 'stack.h' and 'basic_node.h' to 'Structures/include/'

---
+++ Structures/include/basic_node.h
@@ -0,0 +1,62 @@
+/***********************************************************************************//**
+ * Author:       Steven Ward <stevenward94@gmail.com>
+ * File:         csci-2103/Structures/include/basic_node.h
+ * URL:          https://github.com/StevenWard94/csci-2103
+ * Last Change:  2016 Oct 25
+ ***********************************************************************************/
+
+#ifndef STRUCTURES_INCLUDE_BASE_BASIC_NODE_H_
+#define STRUCTURES_INCLUDE_BASE_BASIC_NODE_H_
+
+#include <utility>
+
+namespace base {
+
+template<class E>
+struct basic_node {
+    using value_t = E;
+
+    value_t data;
+    basic_node* next;
+
+    inline value_t const& copy_data( ) const { return value_t(this->data); }
+
+    inline basic_node(value_t const& val = value_t(), basic_node const* nxt = nullptr)
+            : data(val), next(nxt) { }
+
+    /* copy semantics */
+    inline basic_node(basic_node const& other) : data(other.data), next(other.next) { }
+    inline basic_node(basic_node const* const other) : basic_node(*other) { }
+
+    inline basic_node& operator=(basic_node const& other) {
+        if (this != &other) {
+            data = other.data;
+            delete next;
+            next = other.next;
+        }
+        return *this;
+    }
+    inline basic_node& operator=(basic_node const * const other) { return this->operator=(*other); }
+
+    /* move semantics */
+    inline basic_node(basic_node&& other) : data(std::move(other.data)), next(other.next) {
+        other.data = value_t();
+        other.next = nullptr;
+    }
+
+    inline basic_node& operator=(basic_node&& other) {
+        swap(*this, other);
+        return *this;
+    }
+
+
+
+    friend void swap(basic_node& lhs, basic_node& rhs) {
+        std::swap(lhs.data, rhs.data);
+        std::swap(lhs.next, rhs.next);
+    }
+};
+
+} /* end namespace base */
+
+#endif  // (STRUCTURES_INCLUDE_BASE_BASIC_NODE_H_)
--- Structures/include/stack.h
+++ Structures/include/stack.h
@@ -1,19 +1,49 @@
-/***************************************************************************//**
+/***********************************************************************************//**
  * Author:       Steven Ward <stevenward94@gmail.com>
- * File:         <repository-root-dir>/Structures/include/stack.h
+ * File:         csci-2103/Structures/include/stack.h
  * URL:          https://github.com/StevenWard94/csci-2103
- * Last Change:  2016 Oct 18
- ****************************************************************************/
-#ifndef STRUCTURES_INCLUDE_STACKS_STACK_H_
-#define STRUCTURES_INCLUDE_STACKS_STACK_H_
+ * Last Change:  2016 Oct 26
+ ***********************************************************************************/
 
+#ifndef STRUCTURES_INCLUDE_BASE_STACK_H_
+#define STRUCTURES_INCLUDE_BASE_STACK_H_
+
+#include <utility>
 #include <stddef.h>
 
-namespace stacks {
+namespace base {
+
+template<typename T, template<typename> class Node>
+class stack {
+    using value_t = T;
+    using node_t = Node<T>;
+
+  public:
+    inline value_t const& top( ) const { return top_; }
+    inline size_t size( ) const { return size_; }
+
+  protected:
+    inline stack(node_t const* const top = nullptr, size_t size = 0)
+            : top_(top), size_(size) { }
+
+    inline stack(stack const& other) : top_(*other.top_), size_(other.size_) { }
+
+    inline stack(stack&& other)
+            : top_(std::move(*other.top_)), size_(std::move(other.size_))
+    {
+        other.top_ = nullptr;
+        other.size_ = size_t();
+    }
+
+    inline void top(node_t const* const node) { this.top_ = new node_t(node); }
+    inline void size(size_t size) { this.size_ = size; }
+
+  private:
+    node_t* top_;
+    size_t size_;
+};
+
+}  /* end namespace base */
 
 
-
-}
-
-
-#endif  // (STRUCTURES_INCLUDE_STACKS_STACK_H_)
+#endif  // (STRUCTURES_INCLUDE_BASE_STACK_H_)
